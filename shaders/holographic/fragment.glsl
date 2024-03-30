24257ef3e86965aeeaf5abe2df4789e71bcf807b
Tzepes <andreicristian.vlad@gmail.com>
2024-03-30 22:33:16 +0200

    added fresnel effect

--- shaders/holographic/fragment.glsl
+++ shaders/holographic/fragment.glsl
@@ -1,14 +1,27 @@
 uniform float uTime;
 
 varying vec3 vPosition;
+varying vec3 vNormal;
+
 void main() {
+    // Normal
+    vec3 normal = normalize(vNormal);
 
     // Stripes
     float stripes = mod((vPosition.y - uTime * 0.001) * 5.0, 1.0);
     stripes = pow(stripes, 2.0);
 
+    // Fresnel
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    float fresnel = dot(viewDirection, normal) + 1.0;
+    fresnel = pow(fresnel, 2.0);
+
+    // Holograpgic effect
+    float holographic = stripes * fresnel;
+    holographic += fresnel * 1.25;
+
     //final color
-    gl_FragColor = vec4(1.0, 1.0, 1.0, stripes);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, holographic);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 
